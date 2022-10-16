ddfc90514252ca4b6e2a1357d98c06e6e9b4d29e
liontting <wjdaud13580@hanyang.ac.kr>
2022-10-16 21:33:54 +0900

    18108

---
+++ 18108.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int y;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> y;
+	cout << y - (2541 - 1998);
+	return 0;
+}
