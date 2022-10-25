deba8e5338bd80acd78deafad86d79fc75bfb5ae
liontting <wjdaud13580@hanyang.ac.kr>
2022-10-25 21:33:03 +0900

    forget

---
+++ 18301.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, n1, n2, n12;
+	cin >> n1 >> n2 >> n12;
+	N = (n1 + 1)*(n2 + 1) / (n12 + 1) - 1;
+	cout << N;
+	return 0;
+}
