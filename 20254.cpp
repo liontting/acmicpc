6b0336270ce56d7245de18a6de49c015079aeae9
liontting <wjdaud13580@hanyang.ac.kr>
2022-10-26 21:39:41 +0900

    dontknow

---
+++ 20254.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int ur, tr, uo, to;
+	cin >> ur >> tr >> uo >> to;
+	cout << 56 * ur + 24 * tr + 14 * uo + 6 * to;
+	return 0;
+}
