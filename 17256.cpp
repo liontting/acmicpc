950834268e5bac9d8c6f9bdda8f5f8d509f35c92
liontting <wjdaud13580@hanyang.ac.kr>
2022-10-27 21:30:10 +0900

    last

---
+++ 17256.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int ax, ay, az, cx, cy, cz, bx, by, bz;
+	cin >> ax >> ay >> az;
+	cin >> cx >> cy >> cz;
+	bx = cx - az;
+	by = cy / ay;
+	bz = cz - ax;
+	cout << bx << " " << by << " " << bz;
+	return 0;
+}
