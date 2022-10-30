6eacaba4495ba01be191f99df8ed3c92fbb8cc33
liontting <wjdaud13580@hanyang.ac.kr>
2022-10-30 23:55:55 +0900

    18asd

---
+++ 18409.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int N, cnt;
+string S;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	cin >> S;
+	for (int i = 0; i < N; i++)
+		if (S[i] == 'a' || S[i] == 'e' || S[i] == 'i' || S[i] == 'o' || S[i] == 'u')
+			cnt++;
+	cout << cnt;
+	return 0;
+}
