1292522d9512b1a1a42f68de0664ee39421c5d36
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-08 22:35:30 +0900

    21300

---
+++ 21300.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int sum;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	for (int i = 0; i < 6; i++) {
+		int n;
+		cin >> n;
+		sum += n;
+	}
+	cout << sum * 5;
+	return 0;
+}
