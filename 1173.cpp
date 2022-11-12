43c6150ff60db9cd6d69c33df8e4892147c32778
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-12 22:41:09 +0900

    1173

---
+++ 1173.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int N, m, M, T, R, answer, time, check, now;
+
+int main() {
+	cin >> N >> m >> M >> T >> R;
+	now = m;
+	if (now + T > M) {
+		cout << "-1";
+		return 0;
+	}
+	while (time < N) {
+		if (now + T <= M) {
+			answer++;
+			time++;
+			now += T;
+		}
+		else if (now - R >= m) {
+			answer++;
+			now -= R;
+		}
+		else {
+			answer++;
+			now = m;
+		}
+	}
+	cout << answer;
+	return 0;
+}
