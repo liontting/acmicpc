fe68008523fa73cc3b29bc3ad565769f133ddccf
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-15 14:01:41 +0900

    1417

---
+++ 1417.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int N, dasom, answer;
+priority_queue<int> pq;
+
+int main() {
+	cin >> N;
+	cin >> dasom;
+	for (int i = 0; i < N - 1; i++) {
+		int candidate;
+		cin >> candidate;
+		pq.push(candidate);
+	}
+	while (!pq.empty() && dasom <= pq.top()) {
+		int top = pq.top();
+		pq.pop();
+		dasom++;
+		top--;
+		answer++;
+		pq.push(top);
+	}
+	cout << answer;
+	return 0;
+}
