4e448d6145502d041c1e1f850f796b3f8a6f5bc4
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-15 14:55:01 +0900

    1362

---
+++ 1362.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int o, w, n, scenario;
+char m;
+int main() {
+	while (1) {
+		int check = 0;
+		scenario++;
+		cin >> o >> w;
+		if (!o && !w)
+			break;
+		while (1) {
+			cin >> m >> n;
+			if (m == 'E') {
+				w -= n;
+				if (w <= 0)
+					check = 1;
+			}
+			else if (m == 'F')
+				w += n;
+			else
+				break;
+		}
+		cout << scenario << " ";
+		if (w <= 0 || check == 1)
+			cout << "RIP\n";
+		else if (w > o / 2 && w < 2 * o)
+			cout << ":-)\n";
+		else
+			cout << ":-(\n";
+	}
+	return 0;
+}
