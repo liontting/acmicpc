0e66595e0db830c4dd1fec992b729951f1b6f3e2
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-15 16:17:53 +0900

    1004

---
+++ 1004.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int T, x1, y, x2, y2, n;
+
+int main() {
+	cin >> T;
+	while (T--) {
+		int answer = 0;
+		cin >> x1 >> y >> x2 >> y2;
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			int cx, cy, r, c1 = 0, c2 = 0;
+			cin >> cx >> cy >> r;
+			if ((pow(abs(cx - x1), 2) + pow(abs(cy - y), 2)) <= pow(r, 2))
+				c1 = 1;
+			if ((pow(abs(cx - x2), 2) + pow(abs(cy - y2), 2)) <= pow(r, 2))
+				c2 = 1;
+			if (c1 != c2)
+				answer++;
+		}
+		cout << answer << "\n";
+	}
+	return 0;
+}
