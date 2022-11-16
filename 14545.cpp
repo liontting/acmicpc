a406645866e1dccace5bf747a8278f88ca1283c1
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-16 18:46:50 +0900

    14545

---
+++ 14545.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int P, I;
+
+int main() {
+	cin >> P;
+	while (P--) {
+		int answer = 0;
+		cin >> I;
+		for (int i = 1; i <= I; i++)
+			answer += i * i;
+		cout << answer << "\n";
+	}
+	return 0;
+}
