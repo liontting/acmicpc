badb858b33659a70da9cfa0ff29cb5aa489f3857
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-16 17:12:55 +0900

    6768

---
+++ 6768.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int J;
+
+int main() {
+	cin >> J;
+	if (J < 4)
+		cout << "0";
+	else {
+		cout << ((J - 1)*(J - 2)*(J - 3)) / (3 * 2);
+	}
+	return 0;
+}
