0bda855a198a944369bdaf726b9b6264a844b04e
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-16 12:14:48 +0900

    1297

---
+++ 1297.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+double D, H, W, x;
+
+int main() {
+	cin >> D >> H >> W;
+	x = sqrt((D * D) / (H * H + W * W));
+	cout << (int)(H * x) << " " << (int)(W * x);
+	return 0;
+}
