54e01e951ae0b6fef67602fd79fc2ab640488819
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-16 18:23:31 +0900

    11368

---
+++ 11368.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int C, W, L, P;
+
+int main() {
+	while (1) {
+		int answer = 1;
+		cin >> C >> W >> L >> P;
+		if (!C && !W && !L && !P)
+			break;
+		for (int i = 0; i < W*L*P; i++)
+			answer *= C;
+		cout << answer << "\n";
+	}
+	return 0;
+}
