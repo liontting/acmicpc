fa6e011b66a0650aac8a98df67f6f4b92ea9e6fa
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-16 17:04:53 +0900

    24860

---
+++ 15439.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+int N;
+
+int main() {
+	cin >> N;
+	cout << N * (N - 1);
+	return 0;
+}
---
+++ 24860.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+long long int vk, jk, v, j, vh, dh, jh;
+
+int main() {
+	cin >> vk >> jk;
+	cin >> v >> j;
+	cin >> vh >> dh >> jh;
+	cout << (vk * jk + v * j)*vh*dh*jh;
+	return 0;
+}
