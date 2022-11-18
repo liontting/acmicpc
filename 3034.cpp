b184be69d47632cfab45af967d952d90ee7dc46c
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-18 23:09:47 +0900

    3034

---
+++ 3034.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int N, W, H;
+
+int main() {
+	cin >> N >> W >> H;
+	while (N--) {
+		int matches;
+		cin >> matches;
+		if (pow(matches, 2) > (pow(W, 2) + pow(H, 2)))
+			cout << "NE\n";
+		else
+			cout << "DA\n";
+	}
+	return 0;
+}
