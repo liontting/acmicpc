335d2dde66fbb133fce647ec9e37135a94ec5057
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-18 14:44:35 +0900

    11758

---
+++ 11758.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int x[5], y[5];
+double answer;
+
+int main() {
+	for (int i = 0; i < 3; i++)
+		cin >> x[i] >> y[i];
+	x[3] = x[0];
+	y[3] = y[0];
+	for (int i = 0; i < 3; i++)
+		answer += (x[i] * y[i + 1]) - (x[i + 1] * y[i]);
+	if (answer / 2 > 0)
+		cout << "1";
+	else if (answer / 2 < 0)
+		cout << "-1";
+	else
+		cout << "0";
+	return 0;
+}
