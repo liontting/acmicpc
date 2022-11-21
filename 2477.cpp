9e50c65f86a620bc702f5715891657c8a29ced46
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-21 19:29:58 +0900

    2477

---
+++ 2477.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int K, direction, length[10], area[10], maxi, maxarea;
+
+int main() {
+	cin >> K;
+	for (int i = 0; i < 6; i++)
+		cin >> direction >> length[i];
+	length[6] = length[0];
+	for (int i = 0; i < 6; i++) {
+		area[i] = length[i] * length[i + 1];
+		if (maxarea < area[i]) {
+			maxarea = area[i];
+			maxi = i;
+		}
+	}
+	cout << K * (maxarea - area[(maxi + 3) % 6]);
+	return 0;
+}
