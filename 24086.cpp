353c86a9957fe7247b70e64e2a9bd4143a8a8081
liontting <wjdaud13580@hanyang.ac.kr>
2022-11-27 21:09:08 +0900

    24086

---
+++ 24086.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int A, B;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> A;
+	cin >> B;
+	cout << abs(A - B);
+	return 0;
+}
