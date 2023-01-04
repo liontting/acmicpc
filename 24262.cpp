681185a1ab843da81d23012c3eb40472092629b0
liontting <wjdaud13580@hanyang.ac.kr>
2023-01-04 23:41:58 +0900

    24262

---
+++ 24262.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;
+	cout << "1\n0";
+	return 0;
+}
