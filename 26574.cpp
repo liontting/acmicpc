88d631d147faf6b6a36773415c4bea724ae47f9d
liontting <wjdaud13580@hanyang.ac.kr>
2023-01-14 22:57:24 +0900

    26574

---
+++ 26574.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int n, num;
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		cout << num << " " << num << "\n";
+	}
+	return 0;
+}
