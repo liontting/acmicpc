2936f5db37f10f25b7d4d9e84a208f09c425ac55
liontting <wjdaud13580@hanyang.ac.kr>
2023-04-18 22:32:23 +0900

    27323

---
+++ 27323.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int A, B;
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+	cin >> A;
+	cin >> B;
+	cout << A * B;
+	return 0;
+}
