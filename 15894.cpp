0c70ea6cabc61f2a8b6c499734cb05f74a081ee8
liontting <wjdaud13580@hanyang.ac.kr>
2023-04-19 15:50:22 +0900

    15894

---
+++ 15894.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+long long n;
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+	cin >> n;
+	cout << 4*n;
+	return 0;
+}
