aa36d7da96ad30a53752aee64df0e47e7dc12b55
liontting <wjdaud13580@hanyang.ac.kr>
2023-11-14 17:04:44 +0900

    24479

---
+++ 24479.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int N, M, R, visited[100010], num = 1;
+vector<int> e[100010];
+
+void dfs(int x) {
+	for (auto iter = e[x].begin(); iter < e[x].end(); iter++) {
+		if (!visited[*iter]) {
+			visited[*iter] = num++;
+			dfs(*iter);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N >> M >> R;
+	for (int i = 0; i < M; i++) {
+		int u, v;
+		cin >> u >> v;
+		e[u].push_back(v);
+		e[v].push_back(u);
+	}
+	for (int i = 1; i <= N; i++)
+		sort(e[i].begin(), e[i].end());
+	visited[R] = num++;
+	dfs(R);
+	for (int i = 1; i <= N; i++)
+		cout << visited[i] << "\n";
+	return 0;
+}
