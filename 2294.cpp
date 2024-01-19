e5192abee980c9f212b98ab493ed6358b078e30e
liontting <wjdaud13580@hanyang.ac.kr>
2024-01-19 23:15:07 +0900

    2294

---
+++ 2294.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, k, coin[110], dp[20010];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+		cin >> coin[i];
+	sort(coin, coin + n);
+	for (int i = 1; i <= k; i++)
+		dp[i] = 2 * k;
+	for (int i = 0; i < n; i++)
+		if (coin[i] <= k)
+			for (int j = coin[i]; j <= k; j++)
+				dp[j] = min(dp[j], dp[j - coin[i]] + 1);
+	if (dp[k] == 2 * k)
+		cout << "-1";
+	else
+		cout << dp[k];
+	return 0;
+}
