ca7d5626493974a07943b86553cb059a358ffd99
liontting <wjdaud13580@hanyang.ac.kr>
2024-01-26 21:16:40 +0900

    1890

---
+++ 1890.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int N, game[110][110];
+int dx[2] = { 0,1 }, dy[2] = { 1,0 };
+long long int dp[110][110];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> game[i][j];
+	dp[0][0] = 1;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (i == N - 1 && j == N - 1)
+				break;
+			for (int k = 0; k < 2; k++) {
+				int x = i + game[i][j] * dx[k], y = j + game[i][j] * dy[k];
+				if (x >= 0 && x < N && y >= 0 && y < N)
+					dp[x][y] += dp[i][j];
+			}
+		}
+	}
+	cout << dp[N - 1][N - 1];
+	return 0;
+}
