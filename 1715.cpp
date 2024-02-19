a8e9179be89864673bdf9904d1e349685b050ffd
liontting <wjdaud13580@hanyang.ac.kr>
2024-02-19 22:55:01 +0900

    1715

---
+++ 1715.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int N, sum;
+priority_queue <int> pq;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+		pq.push(-num);
+	}
+	while (pq.size() != 1) {
+		int first = pq.top();
+		pq.pop();
+		int second = pq.top();
+		pq.pop();
+		sum += first + second;
+		pq.push(first + second);
+	}
+	cout << -sum;
+	return 0;
+}
